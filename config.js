febe0b57109acb5188e3d438af740912c05c3de1
Caio Belmont <64670788+BelmontBeta@users.noreply.github.com>
2026-08-04 08:37:12 -0300

    Add new provas with updated details and schedule

--- config.js
+++ config.js
@@ -73,12 +73,16 @@ var CONFIG = {
     ],
   
     provas: [
-      { data: "2026-10-05", disciplina: "Lógica Matemática para Computação",                 tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "Desconhecido · (Aguardando)" },
-      { data: "2026-10-06", disciplina: "Fundamentos de Desenvolvimento de Software",        tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "Desconhecido · (Aguardando)" },
-      { data: "2026-10-07", disciplina: "Programação Imperativa e Funcional",                tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "Desconhecido · (Aguardando)" },
-      { data: "2026-10-08", disciplina: "Interfaces Humano Computador",                      tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "Desconhecido · (Aguardando)" },
-      { data: "2026-10-09", disciplina: "FP2: Gestão de Projetos",                           tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "Desconhecido · (Aguardando)" },
-      { data: "2026-10-10", disciplina: "Projeto 02",                                        tipo: "Status Report 1", horario: "A confirmar", sala: "Desconhecido · (Aguardando)" },
+      { data: "2026-08-20", disciplina: "Programação Imperativa e Funcional",                tipo: "Mini-Prova 01 · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },
+      { data: "2026-09-10", disciplina: "Programação Imperativa e Funcional",                tipo: "Mini-Prova 02 · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },
+      { data: "2026-09-29", disciplina: "Programação Imperativa e Funcional",                tipo: "Mini-Prova 03 · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },
+      { data: "2026-09-10", disciplina: "Programação Imperativa e Funcional",                tipo: "Lançamento de Projeto · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },  
+      { data: "2026-10-05", disciplina: "Lógica Matemática para Computação",                 tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },
+      { data: "2026-10-06", disciplina: "Fundamentos de Desenvolvimento de Software",        tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },
+      { data: "2026-10-07", disciplina: "Programação Imperativa e Funcional",                tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },
+      { data: "2026-10-08", disciplina: "Interfaces Humano Computador",                      tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },
+      { data: "2026-10-09", disciplina: "FP2: Gestão de Projetos",                           tipo: "AV1 · 2ª Unidade", horario: "A confirmar", sala: "104 · Brum" },
+      { data: "2026-10-10", disciplina: "Projeto 02",                                        tipo: "Status Report 1", horario: "A confirmar", sala: "104 · Brum" },
     ],
   
     monitorias: [
